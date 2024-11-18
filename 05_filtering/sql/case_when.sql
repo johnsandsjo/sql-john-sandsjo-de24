d91b895c8ec5e9e47e435cb5ec4e6f08e7d329ca
John Sandsjö <john.sandsjo@gmail.com>
2024-11-18 14:02:56 +0100

    improving query

--- 05_filtering/sql/case_when.sql
+++ 05_filtering/sql/case_when.sql
@@ -1,22 +1,25 @@
-SELECT * FROM main.DATA_JOBS dj ;
+SELECT * FROM main.data_jobs;
 
 SELECT
-	CASE 
+	CASE
 		WHEN experience_level = 'SE' THEN 'Senior'
-		WHEN experience_level = 'MI' THEN 'Mid Level'
 		WHEN experience_level = 'EN' THEN 'Entry Level'
+		WHEN experience_level = 'MI' THEN 'Mid Level'
 		WHEN experience_level = 'EX' THEN 'Expert'
-	END AS expereince_level, * EXCLUDE(experience_level)
-FROM
-	main.DATA_JOBS dj ;
-	
-UPDATE main.DATA_JOBS 
-SET 
-	experience_level = CASE 
+	END AS experience_level, * EXCLUDE(experience_level)
+	FROM main.data_jobs;
+
+UPDATE main.data_jobs 
+SET experience_level = CASE
 		WHEN experience_level = 'SE' THEN 'Senior'
-		WHEN experience_level = 'MI' THEN 'Mid Level'
 		WHEN experience_level = 'EN' THEN 'Entry Level'
+		WHEN experience_level = 'MI' THEN 'Mid Level'
 		WHEN experience_level = 'EX' THEN 'Expert'
+		ELSE experience_level --Preserve existing VALUES IF NO match
 	END;
+	WHERE experience_level IN ('SE', 'MI', 'EN', 'EX');
+
+--make sure to add ELSE and WHERE here so that we do not over write.
+
 	
-SELECT DISTINCT experience_level FROM main.DATA_JOBS dj ;
+SELECT DISTINCT experience_level FROM main.DATA_JOBS;
--- 05_filtering/sql/where_clause.sql
+++ 05_filtering/sql/where_clause.sql
@@ -19,7 +19,7 @@ SELECT
 FROM
 	main.DATA_JOBS dj
 WHERE
-	experience_level = 'EN';
+	experience_level = 'Entry Level';
 
 SELECT
 	experience_level , MEDIAN(salary_in_usd) AS median_salary_usd
@@ -27,3 +27,12 @@ FROM
 	main.DATA_JOBS dj
 GROUP BY
 	experience_level;
+
+
+SELECT
+	job_title , MEDIAN(salary_in_usd) as median_salary, ROUND(MEDIAN(salary_in_usd) /12*11,-1) as median_salary_monthly_sek
+FROM
+	main.DATA_JOBS dj
+	WHERE job_title in('Data Product Manager','Product Manager', 'Data Engineer')
+	GROUP BY job_title
+	ORDER BY median_salary_monthly_sek DESC;
