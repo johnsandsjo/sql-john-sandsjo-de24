6a19d87d0c085845ae9e4ffe73b197e321cc8b62
John Sandsjö <john.sandsjo@gmail.com>
2024-11-20 12:51:28 +0100

    chnaged folder name

--- exercises/exercise_1/e_1_0/sql/encore_ingest_data.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE 
-    IF NOT EXISTS encore_salaries AS (
-    SELECT * FROM read_csv_auto('data/salaries.csv')
-);
--- exercises/exercise_1/e_1_0/sql/ingest_data.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE 
-    IF NOT EXISTS cleaned_salaries AS (
-        SELECT * 
-        FROM read_csv_auto('data/salaries.csv')
-    );
