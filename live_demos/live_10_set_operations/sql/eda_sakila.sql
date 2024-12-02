fc1d8975870a8fd4a0725aced4cdb644dc4361f9
John Sandsjö <john.sandsjo@gmail.com>
2024-12-02 12:47:12 +0100

    live demos

--- live_demos/live_10_set_operations/sql/eda_sakila.sql
+++ live_demos/live_10_set_operations/sql/eda_sakila.sql
@@ -1,3 +1,28 @@
 DESC;
 
 SELECT * FROM main.film;
+
+DESC TABLE main.film;
+
+--update ratings to Swedish ratings
+SELECT DISTINCT rating FROM main.film;
+
+ALTER TABLE main.film ADD COLUMN swe_rating STRING;
+
+CREATE TABLE main.film_swe AS (
+	SELECT * FROM main.film
+);
+
+SELECT * FROM main.film_swe;
+
+ALTER TABLE main.film_swe ADD COLUMN swe_rating STRING;
+
+
+---
+SELECT 'CUSTOMER' AS type, c.first_name, c.last_name FROM main.customer c WHERE c.first_name LIKE 'A%'
+UNION ALL
+SELECT 'ACTOR' AS type, a.first_name, a.last_name FROM main.actor a WHERE a.first_name LIKE 'A%';
+
+SELECT c.first_name, c.last_name FROM main.customer c
+INTERSECT
+SELECT a.first_name, a.last_name FROM main.actor a;
