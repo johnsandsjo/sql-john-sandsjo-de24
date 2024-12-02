52a0dd49c3f1ba18979d8b14168f5711614b7dd5
John Sandsjö <john.sandsjo@gmail.com>
2024-12-02 11:00:49 +0100

    set operations live demos

---
+++ live_demos/live_10_set_operations/sql/load_sakila.sql
@@ -0,0 +1,4 @@
+INSTALL sqlite;
+LOAD sqlite;
+
+CALL sqlite_attach ('/Users/john.sandsjo/Documents/github/sql-john-sandsjo-de24/live_demos/live_10_set_operations/data/sqlite-sakila.db')
---
+++ live_demos/live_10_set_operations/sql/set_operations.sql
@@ -0,0 +1,25 @@
+SELECT * FROM synthetic.sales_jan
+UNION
+SELECT * FROM synthetic.sales_feb;
+
+SELECT product_name, amount FROM synthetic.sales_jan
+UNION
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+SELECT product_name, amount FROM synthetic.sales_jan
+UNION ALL
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+SELECT product_name, amount FROM synthetic.sales_jan
+INTERSECT
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+SELECT product_name, amount FROM synthetic.sales_jan
+EXCEPT
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+
+--returns empty due to only unique data
+SELECT * FROM synthetic.sales_jan
+INTERSECT
+SELECT * FROM synthetic.sales_feb;
